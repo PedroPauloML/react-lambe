2f7481d6f24a2a25f220926b312739ae8828ff29
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-27 16:59:06 -0300

    Using Firebase Storage #03

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const cors = require("cors")({ origin: true })
-const fs = requure("fs")
+const fs = require("fs")
 const uuid = require("uuid-v4")
 const { Storage } = require("@google-cloud/storage")
 const storage = new Storage({
@@ -11,8 +11,39 @@ const storage = new Storage({
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.uploadImage = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    
+    try {
+      fs.writeFileSync(
+        "/tmp/imageToSave.jpg", // path temp
+        request.body.image, // image file
+        "base64" // enconding type
+      )
+      const buckect = storage.bucket("lambe-e36d9.appspot.com")
+      const id = uuid()
+      bucket.upload("/tmp/imageToSave.jpg", {
+        uploadType: "media",
+        destination: `/posts/${id}.jpg`,
+        metadata: {
+          metadata: {
+            contentType: "image/jpeg",
+            firebaseStorageDownloadTokens: id
+          }
+        }
+      }, (err, file) => {
+        if (err) {
+          console.log(err)
+          return response.status(500).json({ error: err })
+        } else {
+          const filename = encodeURIComponent(file.name)
+          const imageUrl = "https://firebase.googleapis.com/v0/b/"
+            + buckect.name + "/o/" + filename + "?alt=media&token=" + id
+          return response.status(201).json({ imageUrl: imageUrl })
+        }
+      })
+    } catch (err) {
+      console.log(err)
+      return response.status(500).json({ error: err })
+    }
   })
 });
