21df1550505e8c4eef5689e19f3504e04fdb6706
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-27 16:39:51 -0300

    Using Firebase Storage #02

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const cors = require("cors")({ origin: true })
+const fs = requure("fs")
+const uuid = require("uuid-v4")
+const { Storage } = require("@google-cloud/storage")
+const storage = new Storage({
+  projectId: "lambe-e36d9",
+  keyFilename: "lambe-e36d9.json"
+})
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  cors(request, response, () => {
+    
+  })
 });
