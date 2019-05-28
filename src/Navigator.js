13aa1e0f47cc862dd88804f7d352d2ff95087479
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-28 10:17:25 -0300

    Splash screen

--- src/Navigator.js
+++ src/Navigator.js
@@ -10,6 +10,7 @@ import AddPhoto from "./screens/AddPhoto"
 import Profile from "./screens/Profile"
 import Login from "./screens/Login"
 import Register from "./screens/Register"
+import Splash from "./screens/Splash"
 
 const authRouter = createStackNavigator({
   Login: { screen: Login, navigationOptions: { title: "Login" } },
@@ -62,4 +63,11 @@ const MenuConfig = {
 
 const MenuNavigator = createBottomTabNavigator(MenuRoutes, MenuConfig)
 
-export default MenuNavigator
+const SplashRouter = createSwitchNavigator({
+  Splash: Splash,
+  App: MenuNavigator
+}, {
+  initialRouteName: "Splash"
+})
+
+export default SplashRouter
